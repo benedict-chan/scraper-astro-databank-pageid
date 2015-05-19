1af262a0620310b813fb7659c24e779ef9753411
Benedict Chan <purple.enchanted@gmail.com>
2015-05-19 16:37:33 +1000

    Add some exception catching

--- scraper.py
+++ scraper.py
@@ -1,9 +1,10 @@
 import scraperwiki
 import urllib2
 import json, requests
+import pprint
 
 api_url = "http://www.astro.com/wiki/astro-databank/api.php?"
-
+#http://www.astro.com/wiki/astro-databank/api.php?format=json&action=query&list=allpages&aplimit=500&apcontinue=Woodbridge%2C%20Todd
 
 apcontinue = ""
 params = { 
@@ -17,9 +18,12 @@ params.update({"apcontinue":apcontinue})
 for x in range(0, 100):
 	params.update({"apcontinue":apcontinue})
 	resp = requests.get(url=api_url, params=params)
-	results_json = resp.json()
-	apcontinue = results_json["query-continue"]["allpages"]["apcontinue"]
-	all_pages = results_json["query"]["allpages"]
+	try:
+		results_json = resp.json()
+		apcontinue = results_json["query-continue"]["allpages"]["apcontinue"]
+		all_pages = results_json["query"]["allpages"]
+	except Exception, e:
+		apcontinue = "" # break the loop later
 	for page in all_pages:
 		data = {}
 		data = {"id": page["pageid"], "page_id": page["pageid"], "page_title": page["title"]}
