94a589fe7baa080007fef8771074fac483bac6e7
Benedict Chan <purple.enchanted@gmail.com>
2015-05-19 15:57:41 +1000

    Try setup the scraper

--- scraper.py
+++ scraper.py
@@ -14,14 +14,22 @@ params = {
 }
 params.update({"apcontinue":apcontinue})
 
+for x in range(0, 100):
+	params.update({"apcontinue":apcontinue})
+	resp = requests.get(url=api_url, params=params)
+	results_json = resp.json()
+	apcontinue = results_json["query-continue"]["allpages"]["apcontinue"]
+	all_pages = results_json["query"]["allpages"]
+	for page in all_pages:
+		data = {}
+		data = {"id": page["pageid"], "page_id": page["pageid"], "page_title": page["title"]}
+		data["id"] = page["pageid"]
+		data["page_id"] = page["pageid"]
+		data["page_title"] = page["title"]
+		scraperwiki.sqlite.save(unique_keys=['id'], data=data, table_name="data")
+		pass
+	if not apcontinue:
+		break
+	pass
 
-resp = requests.get(url=api_url, params=params)
-results_json = resp.json()
 
-apcontinue = results_json["query-continue"]["allpages"]["apcontinue"]
-
-all_pages = results_json["query"]["allpages"]
-for page in all_pages:
-	page_id = page["pageid"]
-	page_title = page["title"]
-	
