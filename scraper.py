c5ec91f9068db79a698fb607d234c68bf99830a7
Benedict Chan <purple.enchanted@gmail.com>
2015-05-21 12:22:06 +1000

    Try adding a method to actually scrape the page contents and store it in DB, just try several records now

--- scraper.py
+++ scraper.py
@@ -3,6 +3,22 @@ import urllib2
 import json, requests
 import pprint
 
+
+def request_page_using_id ( page_id ):
+	p_params = { 
+	    "format":"json", 
+	    "action":"query", 
+	    "prop":"revisions", 
+	    "rvprop":"content" 
+	}
+	p_params.update({"pageids":page_id})
+	p_api_url = "http://www.astro.com/wiki/astro-databank/api.php?"
+	resp = requests.get(url=p_api_url, params=p_params)
+	single_data = resp.json()
+	json_str = json.dumps(single_data)
+	return json_str
+
+
 api_url = "http://www.astro.com/wiki/astro-databank/api.php?"
 #http://www.astro.com/wiki/astro-databank/api.php?format=json&action=query&list=allpages&aplimit=500&apcontinue=Woodbridge%2C%20Todd
 
@@ -11,11 +27,11 @@ params = {
 	"format":"json", 
 	"action":"query", 
 	"list":"allpages", 
-	"aplimit":"500" 
+	"aplimit":"10" 
 }
 params.update({"apcontinue":apcontinue})
 
-for x in range(0, 100):
+for x in range(0, 1):
 	params.update({"apcontinue":apcontinue})
 	resp = requests.get(url=api_url, params=params)
 	try:
@@ -30,6 +46,7 @@ for x in range(0, 100):
 		data["id"] = page["pageid"]
 		data["page_id"] = page["pageid"]
 		data["page_title"] = page["title"]
+		data["json_str"] = request_page_using_id(page["pageid"])
 		scraperwiki.sqlite.save(unique_keys=['id'], data=data, table_name="data")
 		pass
 	if not apcontinue:
